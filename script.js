62d6665394ddbc43bf7c99d6520cddecb341f9c2
gsgswe123 <chinhan20917976549e@gmail.com>
2025-08-01 09:06:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // script.js (Frontend)
-const API_BASE_URL = 'https://cors-anywhere.herokuapp.com/http://194.62.248.81:2296/api/v1';
+const API_BASE_URL = 'https://shop-4mlk.onrender.com//api/v1';
 
 // Global variables
 let currentUser = null;
